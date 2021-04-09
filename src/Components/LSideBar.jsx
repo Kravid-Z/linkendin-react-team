bc40e9734e97b7a7818bbb77f369c01f52c3017a
Can Soylu <hsyncn17@gmail.com>
2021-04-09 15:45:20 +0300

    fixed some stuff

--- src/Components/LSideBar.jsx
+++ src/Components/LSideBar.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Card, ListGroup } from "react-bootstrap";
 import styles from "../styles/jumbotron_profile_page_style.module.css";
-import LSidebarSecond from './LSidebarSecond'
-
+import LSidebarSecond from "./LSidebarSecond";
 
 export default class LSideBar extends Component {
   state = {
     userData: {},
   };
-
 
   // getMyData = async () => {
   //   const hafizToken =
@@ -35,7 +33,6 @@ export default class LSideBar extends Component {
   //   }
   // };
 
-
   // componentDidMount = () => {
   //   this.getMyData()
   // };
@@ -45,58 +42,58 @@ export default class LSideBar extends Component {
       <>
         <Card>
           <Card.Img
-            variant='top'
-            src='https://picsum.photos/800/300'
+            variant="top"
+            src="https://picsum.photos/800/300"
             style={{
-              borderTopLeftRadius: '15px',
-              borderTopRightRadius: '15px',
+              borderTopLeftRadius: "15px",
+              borderTopRightRadius: "15px",
             }}
           />
           <Card.Body>
             <div
-              className='styles.imgWrapper ml-4'
-              style={{ marginTop: '-60px' }}
+              className="styles.imgWrapper ml-4"
+              style={{ marginTop: "-60px" }}
             >
               <img
                 className={styles.borderRound}
                 src={this.props.user.image}
-                alt='profile pic'
-                style={({ width: '70px' }, { height: '70px' })}
+                alt="profile pic"
+                style={{ width: "70px", height: "70px" }}
               />
             </div>
             <Card.Title
-              className='mt-2 text-center'
-              style={{ fontSize: '15px' }}
+              className="mt-2 text-center"
+              style={{ fontSize: "15px" }}
             >
               {this.props.user.name} {this.props.user.surname}
-              <div className='text-center muted' style={{ fontSize: '12px' }}>
+              <div className="text-center muted" style={{ fontSize: "12px" }}>
                 {this.props.user.title}
               </div>
             </Card.Title>
 
             <Card.Text
-              className='text-center muted'
-              style={{ fontSize: '12px' }}
+              className="text-center muted"
+              style={{ fontSize: "12px" }}
             >
               {this.props.user.area}
             </Card.Text>
 
-            <ListGroup variant='flush'>
+            <ListGroup variant="flush">
               <ListGroup.Item
-                className='muted ml-1 text-truncate'
-                style={{ fontSize: '9px' }}
+                className="muted ml-1 text-truncate"
+                style={{ fontSize: "9px" }}
               >
                 Who viewd your profile
               </ListGroup.Item>
               <ListGroup.Item
-                className='muted ml-1'
-                style={{ fontSize: '9px' }}
+                className="muted ml-1"
+                style={{ fontSize: "9px" }}
               >
                 Connections
               </ListGroup.Item>
               <ListGroup.Item
-                className='ml-1 text-bold'
-                style={{ fontSize: '9px', fontWeight: 'bold' }}
+                className="ml-1 text-bold"
+                style={{ fontSize: "9px", fontWeight: "bold" }}
               >
                 Grow your Network
               </ListGroup.Item>
@@ -109,8 +106,3 @@ export default class LSideBar extends Component {
     );
   }
 }
-
-
-
-
-
