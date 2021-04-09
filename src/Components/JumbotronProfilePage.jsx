de89e5674b04a7e8b782ac707d135e898df89b59
Can Soylu <hsyncn17@gmail.com>
2021-04-09 14:53:25 +0300

    added modal for changing photo

--- src/Components/JumbotronProfilePage.jsx
+++ src/Components/JumbotronProfilePage.jsx
@@ -1,46 +1,45 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 import styles from "../styles/jumbotron_profile_page_style.module.css";
+import ModalEditProfile from "./ModalEditProfile";
 
 export default function Hero({ thisUser }) {
   return (
     <Card
       style={{
-        borderTopLeftRadius: '15px',
-        borderTopRightRadius: '15px',
-        borderBottomLeftRadius: '15px',
-        borderBottomRightRadius: '15px',
-        width: '100%'
+        borderTopLeftRadius: "15px",
+        borderTopRightRadius: "15px",
+        borderBottomLeftRadius: "15px",
+        borderBottomRightRadius: "15px",
+        width: "100%",
       }}
     >
       <Card.Img
-        variant='top'
-        src='https://picsum.photos/800/300'
-        style={{ borderTopLeftRadius: '15px', borderTopRightRadius: '15px' }}
+        variant="top"
+        src="https://picsum.photos/800/300"
+        style={{ borderTopLeftRadius: "15px", borderTopRightRadius: "15px" }}
       />
       <Card.Body>
         <div className={styles.imgWrapper}>
           <img
             className={styles.borderRound}
             src={thisUser.image}
-            alt='profile pic'
-            style={({ width: '180px' }, { height: '180px' })}
+            alt="profile pic"
+            style={({ width: "180px" }, { height: "180px" })}
           />
         </div>
-        <Card.Title className='mt-2'>
+        <Card.Title className="mt-2">
           {thisUser.name} {thisUser.surname}
         </Card.Title>
         <h6>{thisUser.title}</h6>
         <Card.Text>{thisUser.area}</Card.Text>
-        <Button className='rounded-pill' variant='primary'>
+        <Button className="rounded-pill" variant="primary">
           Open for...
         </Button>
-        <Button className='mx-2 rounded-pill' variant='outline-secondary'>
+        <Button className="mx-2 rounded-pill" variant="outline-secondary">
           Edit Profile
         </Button>
-        <Button className='rounded-pill' variant='outline-secondary'>
-          More...
-        </Button>
+        <ModalEditProfile userId={thisUser._id} />
       </Card.Body>
     </Card>
   );
---
+++ src/Components/ModalEditProfile.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+
+class ModalEditProfile extends React.Component {
+  state = {
+    modalShow: false,
+    selectedFile: null,
+  };
+
+  setModalShowTrue = () => {
+    this.setState({
+      modalShow: true,
+    });
+  };
+
+  setModalShowFalse = () => {
+    this.setState({
+      modalShow: false,
+    });
+  };
+
+  fileSelectedHandler = (e) => {
+    this.setState({
+      selectedFile: e.target.files[0],
+    });
+  };
+
+  postImage = async () => {
+    const fd = new FormData();
+    fd.append("profile", this.state.selectedFile);
+    const andisToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMGM5YzZmZDIyODAwMTUzZmRiYWMiLCJpYXQiOjE2MTc2OTM4NTIsImV4cCI6MTYxODkwMzQ1Mn0.b_4i8l9HxOmAylxIxWyK1cX9Brjnydu_my16UsNd4PE";
+
+    try {
+      let response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${this.props.userId}/picture`,
+        {
+          method: "POST",
+          body: fd,
+          headers: {
+            authorization: "Bearer " + andisToken,
+          },
+        }
+      );
+      if (response.ok) {
+        console.log("files uploaded");
+      } else {
+        console.log("there is something wrong");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  fileUploadHandler = async (e) => {
+    e.preventDefault();
+
+    //console.log(this.state.selectedFile);
+    await this.postImage();
+  };
+
+  //const [show, setShow] = useState(false);
+
+  // const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+
+  render() {
+    return (
+      <>
+        <Button variant="primary" onClick={this.setModalShowTrue}>
+          Launch demo modal
+        </Button>
+
+        <Modal show={this.state.modalShow} onHide={this.setModalShowFalse}>
+          <Form onSubmit={this.fileUploadHandler}>
+            {" "}
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form.Group>
+                <Form.File
+                  accept="image/*"
+                  onChange={this.fileSelectedHandler}
+                  label="Choose an image file"
+                />
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.setModalShowFalse}>
+                Close
+              </Button>
+              <Button variant="primary" type="submit">
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default ModalEditProfile;
